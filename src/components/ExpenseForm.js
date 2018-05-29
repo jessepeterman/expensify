e2291e9456e067f672820a49e6ca488905b27a7c
jessepeterman <jessepeterman@gmail.com>
2018-05-29 12:50:04 -0700

    updated after more jest testing

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -57,6 +57,7 @@ export default class ExpenseForm extends React.Component {
   render() {
     return (
       <div>
+        {this.state.error && <p>{this.state.error}</p>}
         <form onSubmit={this.onSubmit}>
           <input
           type="text"
