c2c983aa3fefd934e389e9af5d7097a3b031ff15
Jesse Peterman <jessepeterman@gmail.com>
2018-05-29 22:09:32 -0700

    finished all the jest tests

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -3,33 +3,42 @@ import {connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
 import {editExpense, removeExpense} from '../actions/expenses';
 
-const EditExpensePage = (props) => {
-  return(
-    <div>
-      <ExpenseForm
-        expense={props.expense}
-        onSubmit={(expense) => {
-          props.dispatch(editExpense(props.expense.id, expense));
-          props.history.push('/');
-        }}  
-        /> 
-        <button
-        expense={props.expense}
-        onClick={(expense) => {
-           props.dispatch(removeExpense({id: props.expense.id}));
-           props.history.push('/');
-        }}>Remove</button>
+export class EditExpensePage extends React.Component {
+  onSubmit = (expense) => {
+    this.props.editExpense(this.props.expense.id, expense);
+    this.props.history.push('/');
+  };
 
-        <p>{props.match.params.id == 66 ? "66 is awesome" : "you're  not cool"}</p>
-  
-  </div>
-  );
-};
+  onClick = (expense) => {
+   this.props.removeExpense({id: this.props.expense.id});
+   this.props.history.push('/');
+  }; 
+
+  render() {
+    return (
+      <div>
+        <ExpenseForm
+        expense={this.props.expense}
+        onSubmit={this.onSubmit}
+      /> 
+        <button
+        expense={this.props.expense}
+        onClick={this.onClick}>Remove</button>
+      </div>
+    )
+  }
+}
 
 const mapStateToProps = (state, props) => {
   return {
     expense: state.expenses.find((expense) => expense.id === props.match.params.id)
     };
   };
+
+const mapDispatchToProps = (dispatch, props) => ({
+  editExpense: (id, expense) => dispatch(editExpense(id, expense)),
+  removeExpense: (expense) => dispatch(removeExpense(expense))
+})
+
   
-export default connect(mapStateToProps)(EditExpensePage);
+export default connect(mapStateToProps, mapDispatchToProps)(EditExpensePage);
