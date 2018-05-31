29f1d0df5237598fe2ba01661136882a0caf22ef
jessepeterman <jessepeterman@gmail.com>
2018-05-31 16:19:21 -0700

    setup production build and server

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,3 @@
-// entry point
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
@@ -19,13 +18,28 @@ module.exports = (env) => {
         exclude: /node_modules/
       }, {
         test: /\.s?css$/,
-        use: [
-            'style-loader',
-            'css-loader',
-            'sass-loader'
+        use: CSSExtract.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true
+              }
+            }
           ]
+        }) 
       }]
     },
+    plugins: [
+      CSSExtract
+    ],
     devtool: isProduction ? 'source-map' : 'inline-source-map',
     devServer: {
       contentBase: path.join(__dirname, 'public'),
