290e6eb15770ce1378f614e34538e5a89e66a1c5
Jesse Peterman <jessepeterman@gmail.com>
2018-06-01 18:43:14 -0700

    added expenses summary total component

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ExpenseList from './ExpenseList';
 import ExpenseListFilters from './ExpenseListFilters';
+import getExpensesTotal from '../selectors/expenses-total';
+import ExpensesSummary from './ExpensesSummary';
 
 const ExpenseDashboardPage = () => (
   <div>
+    <ExpensesSummary />
     <ExpenseListFilters />
     <ExpenseList />
   </div>
