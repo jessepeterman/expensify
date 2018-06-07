81378a722f70670d4a1be369235cb30cae16f0e2
Jesse Peterman <jessepeterman@gmail.com>
2018-06-07 10:16:28 -0700

    added firebase support to actions

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import {addExpense} from './actions/expenses';
+import {startSetExpenses} from './actions/expenses';
 import {setTextFilter, sortByAmount, sortByDate, setStartDate, setEndDate} from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
@@ -13,9 +13,9 @@ import './firebase/firebase';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: "Water Bill", amount: 4500, createdAt: 4500}));
-store.dispatch(addExpense({description: "Gas Bill", amount: 5000, createdAt: 1000}));
-store.dispatch(addExpense({description: "Rent", amount: 109500,}));
+// store.dispatch(addExpense({ description: "Water Bill", amount: 4500, createdAt: 4500}));
+// store.dispatch(addExpense({description: "Gas Bill", amount: 5000, createdAt: 1000}));
+// store.dispatch(addExpense({description: "Rent", amount: 109500,}));
 
 const state = store.getState(); 
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
@@ -26,4 +26,8 @@ const jsx = (
   </Provider>
 );
 
-ReactDOM.render(jsx, document.getElementById('app'));
+ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+
+store.dispatch(startSetExpenses()).then(() => {
+  ReactDOM.render(jsx, document.getElementById('app'));
+});
--- src/tests/reducers/expenses.test.js
+++ src/tests/reducers/expenses.test.js
@@ -1,5 +1,6 @@
 import expensesReducer from '../../reducers/expenses';
 import expenses from '../fixtures/expenses';
+import { setExpenses } from '../../actions/expenses';
 
 test ('should set default state', () => {
   const state = expensesReducer(undefined, {type: '@@INIT'});
@@ -75,4 +76,12 @@ test('should not edit an expense', () => {
   expect(state).toEqual(expenses);
 });
 
+test('should set expenses', () => {
+  const action = {
+    type: 'SET_EXPENSES',
+    expenses: [expenses[1]]
+  };
+  const state = expensesReducer(expenses, action);
+  expect(state).toEqual([expenses[1]]);
+});
 
