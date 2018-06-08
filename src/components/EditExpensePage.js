2e42fd55d7bc77918017d8dfceb8c95959ebfb6a
jessepeterman <jessepeterman@gmail.com>
2018-06-08 14:02:05 -0700

    finished styling the app

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -17,13 +17,20 @@ export class EditExpensePage extends React.Component {
   render() {
     return (
       <div>
-        <ExpenseForm
-        expense={this.props.expense}
-        onSubmit={this.onSubmit}
-      /> 
-        <button
-        expense={this.props.expense}
-        onClick={this.onClick}>Remove</button>
+          <div className="page-header">
+            <div className="content-container">
+              <h1 className="page-header__title">Edit Expense</h1>
+            </div>
+          </div>
+          <div className="content-container">
+          <ExpenseForm
+          expense={this.props.expense}
+          onSubmit={this.onSubmit}
+        /> 
+          <button className="button button--secondary"
+          expense={this.props.expense}
+          onClick={this.onClick}>Remove Expense</button>
+        </div>
       </div>
     )
   }
@@ -31,7 +38,7 @@ export class EditExpensePage extends React.Component {
 
 const mapStateToProps = (state, props) => {
   return {
-    expense: state.expenses.find((expense) => expense.id === props.match.params.id)
+        expense: state.expenses.find((expense) => expense.id === props.match.params.id)
     };
   };
 
--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import { Link } from 'react-router-dom';
 import selectExpensesTotal from '../selectors/expenses-total';
 import selectExpenses from '../selectors/expenses';
 import numeral from 'numeral';
 
 export const ExpensesSummary = (props) => {
+  const itemWord = props.expenses.length > 1 ? 'items' : 'item';
+  const formattedExpensesTotal = numeral(selectExpensesTotal(props.expenses) / 100).format('$0,0.00');
 
   return (
-    <p><strong>Your total for {props.expenses.length} item(s) is: {numeral(selectExpensesTotal(props.expenses) / 100).format('$0,0.00')}</strong></p>
+    <div className="page-header">
+      <div className="content-container">
+        <h1 className="page-header__title">Your total for <span>{props.expenses.length}</span> {itemWord} is: <span>{formattedExpensesTotal}</span></h1>
+          <div className="page-header__actions">
+            <Link className="button" to="/create">Add Expense</Link>
+          </div>
+      </div>
+    </div>
   );
 }
 
